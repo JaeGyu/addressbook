99c15919c0632e6fbf781717e11efa298e30f6c9
jaegyu <jpmlyr@gmail.com>
2019-01-14 18:17:23 +0900

    수정을 위한 모달 개발 완료

--- addressbook_client/src/components/list.vue
+++ addressbook_client/src/components/list.vue
@@ -15,7 +15,7 @@
       </b-row>
       <b-row>
         <b-col>
-          <b-button @click="destroy">삭제</b-button>
+          <b-button @click="showModalOfDeleteConfirm">삭제</b-button>
         </b-col>
         <b-col cols="6">&nbsp;</b-col>
         <b-col>
@@ -30,7 +30,7 @@
           <b-form-checkbox :id="data.item.id.toString()" :value="data.item.id" v-model="deleteChk"></b-form-checkbox>
         </template>
         <template slot="update" slot-scope="data">
-          <b-button @click="update(data.index)">수정</b-button>
+          <b-button @click="update(data.item.id)">수정</b-button>
         </template>
       </b-table>
     </b-container>
@@ -38,14 +38,47 @@
     <!-- 삭제 여부 모달 -->
     <b-modal ref="deleteConfirm" centered id="deleteConfirm" title="경고">
       <p class="my-4">삭제하시겠습니까?</p>
+      <div slot="modal-footer" class="w-100">
+        <b-btn class="float-right" variant="primary" @click="okOfDeleteConfirm">삭제</b-btn>
+        <b-btn class="float-right mr-2" @click="cancelOfDeleteConfirm">취소</b-btn>
+      </div>
     </b-modal>
 
     <!--수정을 위한 모달-->
+    <b-form @submit.prevent="up">
+      <b-modal ref="update" centered id="update" title="변경">
+        <b-container>
+          <b-form-group horizontal label="이름:" label-class="text-sm-right" label-for="name">
+            <b-form-input
+              v-model="updateData.name"
+              id="name"
+              placeholder="성함을 입력하세요"
+              autofocus
+              required
+            ></b-form-input>
+          </b-form-group>
+          <b-form-group horizontal label="주소:" label-class="text-sm-right" label-for="address">
+            <b-form-input v-model="updateData.address" id="address" placeholder="주소를 입력하세요"></b-form-input>
+          </b-form-group>
+          <b-form-group horizontal label="휴대폰:" label-class="text-sm-right" label-for="cellphone">
+            <b-form-input v-model="updateData.cellphone" id="cellphone" placeholder="휴대폰 번호를 입력하세요"></b-form-input>
+          </b-form-group>
+          <b-form-group horizontal label="집전화:" label-class="text-sm-right" label-for="phone">
+            <b-form-input v-model="updateData.phone" id="phone" placeholder="집 전화번호를 입력하세요"></b-form-input>
+          </b-form-group>
+          <b-form-group horizontal label="우편번호:" label-class="text-sm-right" label-for="zipcode">
+            <b-form-input v-model="updateData.zipcode" id="zipcode" placeholder="우편번호를 입력하세요"></b-form-input>
+          </b-form-group>
+        </b-container>
 
+        <div slot="modal-footer">
+          <b-btn type="submit" class="float-right" variant="primary">저장</b-btn>
+          <b-btn class="float-right mr-2" @click="cancelOfUpdateModal">취소</b-btn>
+        </div>
+      </b-modal>
+    </b-form>
   </div>
 </template>
-
-
 
 <script>
 import axios from "axios";
@@ -63,7 +96,6 @@ export default {
       deleteChk: [],
       deleteAllChk: "",
       options: [
-        // { value: null, text: "조회 조건을 선택하세요" },
         { value: "name", text: "성명" },
         { value: "address", text: "주소" }
       ],
@@ -77,16 +109,56 @@ export default {
         { key: "update", label: "수정" }
       ],
       members: [],
-      errorMsg: ""
+      errorMsg: "",
+      updateData: {}
     };
   },
   methods: {
-    showModalOfDeleteConfirm: function(){
+    showModalOfUpdate: function() {
+      this.$refs.update.show();
+    },
+    showModalOfDeleteConfirm: function() {
+      if (this.deleteChk.length == 0) {
+        return;
+      }
       this.$refs.deleteConfirm.show();
     },
+    hideModalOfDeleteConfirm: function() {
+      this.$refs.deleteConfirm.hide();
+    },
+    okOfDeleteConfirm: function() {
+      this.hideModalOfDeleteConfirm();
+      this.destroy();
+    },
+    cancelOfDeleteConfirm: function() {
+      this.hideModalOfDeleteConfirm();
+    },
+    hideModalOfUpdate: function() {
+      this.$refs.update.hide();
+    },
+    cancelOfUpdateModal: function() {
+      this.hideModalOfUpdate();
+    },
     update: function(data) {
-      console.log(data);
-      console.log(this.deleteChk);
+      const result = this.members.filter(mem => {
+        return data === mem.id;
+      });
+      this.updateData = result[0];
+      this.showModalOfUpdate();
+    },
+    up: function() {
+      axios
+        .put(
+          "http://localhost:3000/address/members/" + this.updateData.id,
+          this.updateData
+        )
+        .then(res => {
+          console.log(res);
+          this.hideModalOfUpdate();
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
     pdfDownload: function() {
       console.log("pdf 다운 로드 입니다.");
@@ -95,13 +167,6 @@ export default {
       console.log("엑셀 다운 로드 입니다.");
     },
     destroy: function() {
-      if (this.deleteChk.length == 0) {
-        return;
-      }
-
-      this.showModalOfDeleteConfirm();
-      return;
-
       axios
         .delete("http://localhost:3000/address/members", {
           params: {
@@ -112,6 +177,7 @@ export default {
           }
         })
         .then(res => {
+          this.deleteChk = [];
           this.getAddress();
         })
         .catch(error => {
@@ -143,7 +209,6 @@ export default {
         .get("http://localhost:3000/address/members", {
           headers: {
             Authorization: store.token
-            // Authorization: 'a'
           }
         })
         .then(res => {
